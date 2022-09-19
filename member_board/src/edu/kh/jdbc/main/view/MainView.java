dceb8db865400a14da7680075a42f78735dda522
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-19 17:14:42 +0900

    MainView

--- member_board/src/edu/kh/jdbc/main/view/MainView.java
+++ member_board/src/edu/kh/jdbc/main/view/MainView.java
@@ -13,41 +13,77 @@ public class MainView {
 	
 	private MainService service = new MainService();
 	
+	// 로그인된 회원 정보를 저장한 객체를 참조하는 참조변수
+	private Member loginMember = null;
+	// -> 로그인 X == NULL
+	// -> 로그인 O != NULL
+	
 	/**
 	 * 메인 메뉴 출력 메서드
 	 */
 	public void mainMenu() {
 		
 		int input = -1;
-		
+
 		do {
 			try {
 				
-				System.out.println("\n***** 회원제 게시판 프로그램 *****\n");
-				System.out.println("1. 로그인");
-				System.out.println("2. 회원 가입");
-				System.out.println("0. 프로그램 종료");
+				// ctrl + shift + f : 들여쓰기, 문장 정렬
+				// ctrl + shift + p : 시작,종료 괄호 이동
 				
-				System.out.print("\n메뉴 선택 : ");
-				input = sc.nextInt();
-				sc.nextLine(); // 입력 버퍼 개행문자 제거
-				
+				// 로그인 X 화면
+				if (loginMember == null) {
+
+					System.out.println("\n***** 회원제 게시판 프로그램 *****\n");
+					System.out.println("1. 로그인");
+					System.out.println("2. 회원 가입");
+					System.out.println("0. 프로그램 종료");
+
+					System.out.print("\n메뉴 선택 : ");
+					input = sc.nextInt();
+					sc.nextLine(); // 입력 버퍼 개행문자 제거
+
+					System.out.println();
+
+					switch (input) {
+					case 1: login(); break; // 로그인
+					case 2: signUp(); break;// 회원 가입
+					case 0: System.out.println("프로그램 종료"); break;
+					default: System.out.println("메뉴에 작성된 번호만 입력해주세요.");
+					}
+				} else { // 로그인 O
+					
+					System.out.println("***** 로그인 메뉴 *****");
+					System.out.println("1. 회원 기능");
+					System.out.println("2. 게시판 기능");
+					System.out.println("0. 로그아웃");
+					System.out.println("99. 프로그램 종료");
+					
+					System.out.println("\n메뉴 선택 : ");
+					input = sc.nextInt();
+
 				System.out.println();
-				
-				switch(input) {
+
+				switch (input) {
 				case 1: break;
-				case 2: signUp(); break;
-				case 0: System.out.println("프로그램 종료");break;
-				default : System.out.println("메뉴에 작성된 번호만 입력해주세요.");
+				case 2: break;
+				case 0: loginMember = null; System.out.println("\n[로그아웃 되었습니다.]");
+				input = -1; // do-while문이 종료되지 않도록 0이 아닌 값으로 변경 
+				break; // 로그아웃 == loginMember가 참조하는 객체 없음(== null)
+				// 로그인 == loginMember가 참조하는 객체 존재
+				case 99: System.out.println("프로그램 종료");
+//				input = 0; // -> do-while 조건식을 false로 만듦
+				System.exit(0); // JVM(자바가상머신) 종료, 매개변수는 0, 아니면 오류를 의미
+				break;
+				default: System.out.println("메뉴에 작성된 번호만 입력해주세요.");
 				}
-				
-			} catch(InputMismatchException e) {
+				System.out.println();
+				}
+			} catch (InputMismatchException e) {
 				System.out.println("\n<<입력 형식이 올바르지 않습니다.>>");
 				sc.nextLine(); // 입력 버퍼에 남아있는 잘못된 문자열 제거
 			}
-			
-		} while(input != 0);
-		
+		} while (input != 0);
 	}
 
 	/**
@@ -145,7 +181,64 @@ public class MainView {
 			e.printStackTrace();
 		}
 		
-		
-		
 	}
+	
+	/**
+	 * 로그인 화면
+	 */
+	private void login() {
+		System.out.println("[로그인]");
+
+		System.out.println("아이디 : ");
+		String memberId = sc.next();
+
+		System.out.println("비밀번호 : ");
+		String memberPw = sc.next();
+
+		
+		
+		try {
+			// 로그인 서비스 호출 후 조회 결과를 loginMember에 저장
+			loginMember = service.login(memberId, memberPw);
+			
+			System.out.println();
+			if (loginMember != null) { // 로그인 성공 시
+				System.out.println(loginMember.getMemberName() + "님 환영합니다.");
+
+			} else { // 로그인 실패 시
+				System.out.println("[아이디 또는 비밀번호가 일치하지 않습니다.]");
+			}
+			System.out.println();
+			
+		} catch (Exception e) {
+			System.out.println("\n<<로그인 중 예외 발생>>\n");
+		}
+
+	}
+	
+	/* 회원기능 (Member View, Service, DAO, member-query.xml)
+	 * 1. 내정보 조회
+	 * 2. 회원 목록 조회(아이디, 이름, 성별)
+	 * 3. 내 정보 수정(이름, 성별)
+	 * 4. 비밀번호 변경(현재 비밀번호, 새 비밀번호, 새 비밀번호 확인)
+	 * 5. 회원 탈퇴
+	 * 
+	 * 게시판 기능 (Board View, Service, DAO, board-query,xml)
+	 * 1. 게시글 목록 조회(최근일 내림차순)
+	 * 		(게시글 번호, 제목, 작성자명, 작성일, 조회수, 댓글 수)
+	 * 2. 게시글 상세 조회(게시글 번호 입력 받음)
+	 * 		(게시글 번호, 제목, 내용, 작성자명, 작성일, 조회수,
+	 * 		댓글 목록(작성일 오름차순)
+	 *		2-1. 게시글 수정 (자신의 게시글만)
+	 * 		2-2. 게시글 삭제 (자신의 게시글만)
+	 * 		2-3. 댓글 작성
+	 * 		2-4. 댓글 수정 (자신의 댓글만)
+	 * 		2-5. 댓글 삭제 (자신의 댓글만)
+	 * 3. 게시글 작성(제목, 내용 INSERT)
+	 * -> 작성 성공 시 상세 조회 수행
+	 * 4. 게시글 검색(제목, 내용, 제목+내용, 작성자)
+	 * 
+	 */
+	
+	
 }
