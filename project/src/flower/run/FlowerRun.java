12981bd8fec000d87fd8d0b2ae0c61ffbbba10bb
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-21 17:42:21 +0900

    FlowerRun

---
+++ project/src/flower/run/FlowerRun.java
@@ -0,0 +1,15 @@
+package flower.run;
+
+import flower.view.FlowerView;
+
+public class FlowerRun {
+	public static void main(String[] args) {
+		
+		
+		new FlowerView().mainMenu();		
+		
+		
+		
+	}
+
+}
