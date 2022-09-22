0cdc5591ca3b079629ceba690e745ddf39575a27
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-22 16:15:43 +0900

    BoardView

--- member_board/src/edu/kh/jdbc/board/view/BoardView.java
+++ member_board/src/edu/kh/jdbc/board/view/BoardView.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import edu.kh.jdbc.board.model.service.BoardService;
 import edu.kh.jdbc.board.model.service.CommentService;
 import edu.kh.jdbc.board.model.vo.Board;
+import edu.kh.jdbc.board.model.vo.Comment;
+import edu.kh.jdbc.main.view.MainView;
 
 public class BoardView {
 
@@ -43,7 +45,7 @@ public class BoardView {
 				
 				switch(input) {
 				case 1: selectAllBoard(); break; // 게시글 목록 조회
-				case 2: break;
+				case 2: selectBoard(); break; // 게시글 상세 조회
 				case 3: break;
 				case 4: break;
 				case 0: System.out.println("[로그인 메뉴로 이동합니다.]"); break;
@@ -93,7 +95,54 @@ public class BoardView {
 		
 	}
 	
-	
+	/**
+	 * 게시글 상세 조회
+	 */
+	private void selectBoard() {
+		System.out.println("\n[게시글 상세 조회]\n");
+		
+		try {
+			System.out.println("게시글 번호 입력 : ");
+			int boardNo = sc.nextInt();
+			sc.nextLine();
+			
+			// 게시글 상세 조회 서비스 호출 후 결과 반환 받기
+			Board board = bService.selectBoard(boardNo, MainView.loginMember.getMemberNo());
+											//게시글번호, 로그인한 회원의 회원번호
+											//			-> 자신의 글 조회수 증가 X​
+	        if (board != null) {
+	            System.out.println("--------------------------------------------------------");
+	            System.out.printf("글번호 : %d \n제목 : %s\n", board.getBoardNo(), board.getBoardTitle());
+	            System.out.printf("작성자 : %s | 작성일 : %s  \n조회수 : %d\n",
+	                  board.getMemberName(), board.getCreateDate(), board.getReadCount());
+	           System.out.println("--------------------------------------------------------\n");
+	           System.out.println(board.getBoardContent());
+	           System.out.println("\n--------------------------------------------------------");
+	           
+	           // 댓글 목록
+	           if(!board.getCommentList().isEmpty()) {
+	              for(Comment c : board.getCommentList()) {
+	                 System.out.printf("댓글번호: %d   작성자: %s  작성일: %s\n%s\n",
+	                       c.getCommentNo(), c.getMemberName(), c.getCreateDate(), c.getCommentContent());
+	                 System.out.println(" --------------------------------------------------------");
+	              }
+	           }
+	           
+	           // 댓글 등록, 수정, 삭제
+	           // 수정/삭제 메뉴
+//	           subBoardMenu(board);
+	           
+	           
+	        } else {
+	           System.out.println("\n[해당 번호의 게시글이 존재하지 않습니다.]\n");
+	        }	
+		} catch (Exception e) {
+			System.out.println("\n<<게시글 상세 조회 중 예외 발생>>\n");
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	
 	
