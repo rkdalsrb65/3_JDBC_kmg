0a5a0644a6d7acde4abac309c39f362a2c506555
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-26 15:04:57 +0900

    EmployeeRun

---
+++ project/src/Employee/run/EmployeeRun.java
@@ -0,0 +1,15 @@
+package Employee.run;
+
+import Employee.view.EmployeeView;
+
+public class EmployeeRun {
+	public static void main(String[] args) {
+		
+		
+		new EmployeeView().mainMenu();		
+		
+		
+		
+	}
+
+}
