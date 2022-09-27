c3a47f584a379c09b24d393bd462a7c922018758
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-27 12:11:58 +0900

    ShoppingService

--- project/src/shopping/model/service/ShoppingService.java
+++ project/src/shopping/model/service/ShoppingService.java
@@ -5,13 +5,17 @@ import static common.JDBCTemplate.*;
 import java.sql.Connection;
 import java.util.List;
 
+import shopping.model.dao.ComDAO;
 import shopping.model.dao.ShoppingDAO;
+import shopping.vo.Com;
 import shopping.vo.Shopping;
 
 public class ShoppingService {
 	
 	private ShoppingDAO dao = new ShoppingDAO();
 
+	private ComDAO cDao = new ComDAO();
+	
 	/** 쇼핑몰 목록 조회 서비스
 	 * @return shoppingList
 	 * @throws Exception
@@ -64,11 +68,110 @@ public class ShoppingService {
 				else			rollback(conn);
 			}
 		}
-		
 		close(conn); // 커넥션 반환		
 		
 		return shopping; // 조회 결과 반환
-		
 	}
 
+	/** 게시글 수정 서비스
+	 * @param shopping
+	 * @return result
+	 * @throws Exception
+	 */
+	public int updateShopping(Shopping shopping) throws Exception {
+		
+		Connection conn = getConnection(); // 커넥션 생성
+		
+		// 1. 게시글 수정 DAO 호출
+		int result = dao.updateShopping(conn, shopping);
+		
+		if(result > 0)  commit(conn);
+		else 			rollback(conn);
+		
+		close(conn);
+		
+		return result;
+	}
+
+	/** 게시글 삭제 서비스
+	 * @param shoppingNo
+	 * @return result
+	 * @throws Exception
+	 */
+	public int deleteShopping(int shoppingNo) throws Exception {
+		
+		Connection conn = getConnection(); // 커넥션 생성
+		
+		// 1. 게시글 삭제 DAO 호출
+		int result = dao.deleteShopping(conn, shoppingNo);
+		
+		if(result > 0)  commit(conn);
+		else 			rollback(conn);
+		
+		close(conn);
+		
+		return result;
+	}
+
+	/** 게시글 등록 서비스
+	 * @param shopping
+	 * @return result
+	 * @throws Exception
+	 */
+	public int insertShopping(Shopping shopping) throws Exception {
+		
+		Connection conn = getConnection(); // 커넥션 생성
+		
+		// 게시글 번호 생성 dao 호출
+		// 왜? 동시에 여러 사람이 게시글을 등록하면
+		// 시퀀스가 한번에 증가하여 CURRVAL 구문을 이용하면 문제가 발생함
+		// -> 게시글 등록 서비스를 호출한 순서대로
+		// 미리 게시글 번호를 생성해서 얻어온 다음 이를 이용해 INSERT 진행
+		int shoppingNo = dao.nextShoppingNo(conn);
+		
+		shopping.setShoppingNo(shoppingNo); // 얻어온 다음 번호를 board에 세팅
+		// -> 다음 게시글 번호, 제목, 내용, 회원 번호
+		
+		// 게시글 등록 DAO 호출
+		int result = dao.insertShopping(conn, shopping);
+		
+		if(result > 0) {
+			commit(conn); // 커밋할지
+			
+			result = shoppingNo;
+			// INSERT 성공 시 생성된 게시글 번호(boardNo)를 결과로 반환
+		}
+		else 			rollback(conn); // 롤백할지
+		
+		close(conn); // 커넥션 반환
+		
+		return result;
+	}
+
+	/** 게시글 검색
+	 * @param condition
+	 * @param query
+	 * @return shoppingList
+	 * @throws Exception
+	 */
+	public List<Shopping> searchShopping(int condition, String query) throws Exception {
+		
+		Connection conn = getConnection(); // 커넥션 생성
+		
+		List<Shopping>shoppingList = dao.searchShopping(conn, condition, query);
+		
+		close(conn);
+		
+		return shoppingList;		
+		
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
