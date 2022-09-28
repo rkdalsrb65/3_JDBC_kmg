934e3949dddf7639ceb7360720b7680dcb8231f8
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-28 11:19:51 +0900

    OrderDAO

--- project/src/order/model/dao/OrderDAO.java
+++ project/src/order/model/dao/OrderDAO.java
@@ -1,5 +1,18 @@
 package order.model.dao;
 
+import java.sql.Connection;
+
+import order.vo.Order;
+
 public class OrderDAO {
+
+	public Order requestOrder(Connection conn, int orderNo) throws Exception {
+		
+		
+		
+		
+		
+		return null;
+	}
 
 }
