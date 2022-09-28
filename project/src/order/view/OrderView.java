ba435b5bbec1126a5fb40f45bea4fe30538b9ad7
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-28 11:20:02 +0900

    OrderView

--- project/src/order/view/OrderView.java
+++ project/src/order/view/OrderView.java
@@ -3,7 +3,11 @@ package order.view;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import Employee.view.EmployeeView;
 import order.model.service.OrderService;
+import order.vo.Order;
+import shopping.vo.Com;
+import shopping.vo.Shopping;
 
 public class OrderView {
 
@@ -19,10 +23,8 @@ public class OrderView {
 			try {
 				
 				System.out.println("\n ★ 주문 기능 ★ \n");
-				System.out.println("1. 주문 목록 조회");
-				System.out.println("2. 주문 상세 조회");
-				System.out.println("3. 주문 게시글 작성");
-				System.out.println("4. 주문 게시글 검색");
+				System.out.println("1. 주문 신청");
+				System.out.println("2. 주문 취소");
 				System.out.println("0. 로그인메뉴로 이동");
 				
 				System.out.print("\n메뉴 선택 : ");
@@ -32,10 +34,8 @@ public class OrderView {
 				System.out.println();
 				
 				switch(input) {
-				case 1: /*selectOrder();*/ break; 	// 주문 목록 조회
-				case 2:  break; 	// 주문 상세 조회
-				case 3:  break; 	// 주문 게시글 등록 (삽입)
-				case 4:  break; 	// 주문 게시글 검색
+				case 1: requestOrder(); break; 	// 주문 신청
+				case 2: cancelOrder(); break; 	// 주문 취소
 				case 0: System.out.println("[로그인 메뉴로 이동합니다.]"); break;
 				default : System.out.println("메뉴에 작성된 번호만 입력 해주세요.");
 				}
@@ -50,8 +50,44 @@ public class OrderView {
 		} while(input != 0);		
 		
 	}
-
 	
+	private void requestOrder() {
+		
+		System.out.println("\n[주문 신청]\n");	
+		
+		try {
+			System.out.println("주문 번호 입력 : ");
+			int orderNo = sc.nextInt();
+			sc.nextLine();
+			
+			// 주문 신청 서비스 호출 후 결과 반환 받기
+			Order order = oService.requestOrder(orderNo, EmployeeView.loginEmployee.getEmployeeNo());
+											//게시글번호, 로그인한 회원의 회원번호
+											//			-> 자신의 글 조회수 증가 X​
+	        if (order != null) {
+	            System.out.println("--------------------------------------------------------");
+	            System.out.printf("글번호 : %d \n제목 : %s\n", order.getShoppingNo(), order.getShoppingTitle());
+	            System.out.printf("작성자 : %s | 작성일 : %s  \n조회수 : %d\n",
+	            		order.getEmployeeName(), order.getCreateDate(), order.getReadCount());
+	           System.out.println("--------------------------------------------------------\n");
+	           System.out.println(order.getShoppingContent());
+	           System.out.println("\n--------------------------------------------------------");
+	            
+	        } else {
+	           System.out.println("\n[주문 할 수 없는 번호 입니다.]\n");
+	        }	
+		} catch (Exception e) {
+			System.out.println("\n<<주문 신청 중 예외 발생>>\n");
+			e.printStackTrace();
+		}		
+		
+	}
 	
-	
+	private void cancelOrder() {
+		
+		System.out.println("\n[주문 취소]\n");	
+		
+		
+		
+	}
 }
