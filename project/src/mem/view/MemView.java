a507e34456370a2ab6029d8b1abe279d18b269f3
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-28 15:49:34 +0900

    MemView

--- project/src/mem/view/MemView.java
+++ project/src/mem/view/MemView.java
@@ -31,9 +31,9 @@ public class MemView {
 			try {
 				System.out.println("\n ★ 회원 기능 ★ \n");
 				System.out.println("1. 내 정보 조회");
-				System.out.println("2. 회원 목록 조회(아이디, 이름, 성별)");
-				System.out.println("3. 내 정보 수정(이름, 성별)");
-				System.out.println("4. 비밀번호 변경(현재 비밀번호, 새 비밀번호, 새 비밀번호 확인)");
+				System.out.println("2. 회원 목록 조회");
+				System.out.println("3. 내 정보 수정");
+				System.out.println("4. 비밀번호 변경");
 				System.out.println("5. 회원 탈퇴");
 				System.out.println("0. 메인메뉴로 이동");
 				
